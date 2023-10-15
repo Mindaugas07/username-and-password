45a8dd77b3c97f949180fae34ca05c388db141d7
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-15 14:08:21 +0300

    changed main.py file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 #    username="algirdas", password="fffff"; username="Vytautas", password="45645ertwe"      ;      "username='', password=''; username='', password=''; username='', password=''"
 
+login_atempts = 0
 usernames = input("Please write at least five username and password combinations in a format username='', password=''....etc ")
 usernames_list = usernames.split("; ")
 
@@ -22,16 +23,15 @@ while True:
     user_input = input("Enter your username and password: ")
     user_pass_list = user_input.split(" ")
     username_entered = user_pass_list[0]
-    print(type(username_entered))
     password_entered = user_pass_list[1]
-    print(password_entered)
     if username_entered in full_dict and full_dict[username_entered] == password_entered:
-        print(f"Correct password, you are now logged in as {username_entered}")
+        print(f"Correct password, you are now logged in as {username_entered}, there were {login_atempts} unsuccessful login atempts")
         break
     else:
-        break
+        print("Wrong credentials, please write your username and password again")
+        login_atempts += 1
 
-# print(f"You entered incorect crediantials for username {user_input_name[0] }, enter password and username again")
 
 
-print(full_dict)
+
+
