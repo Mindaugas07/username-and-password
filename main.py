50b9fedfc00e126068c6af6d0e14522c81755567
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-15 13:49:56 +0300

    update main.py file

--- main.py
+++ main.py
@@ -4,34 +4,34 @@
 # check if credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials'
 # and ask to enter password and username again. The program should show numbers of times you tried to enter both credentials.
 
-# "username='algirdas', password='fffff'; username='Vytautas', password='45645ertwe'; "username='', password=''; username='', password=''; username='', password=''"
+#    username="algirdas", password="fffff"; username="Vytautas", password="45645ertwe"      ;      "username='', password=''; username='', password=''; username='', password=''"
 
-usernames = input("Please write at least five username and password combinations in a format 'username="", password="";'")
+usernames = input("Please write at least five username and password combinations in a format username='', password=''....etc ")
 usernames_list = usernames.split("; ")
-print(usernames_list)
+
 usernames_dict = {}
 for combination in usernames_list:
-    splited_combination = combination.split(", ")
-    print(splited_combination)
+    splited_combination = combination.split(",")
     username = splited_combination[0].strip("username=")
-    print(username)
-    password = splited_combination[1].strip("password=")
-    print(password)
+    password = splited_combination[1].strip(" password=")
     usernames_dict[username] = password
-    # username_dict[]
+
+full_dict = {key.replace('"', ''):val.replace('"', '') for key, val in usernames_dict.items()}    
 
 while True:
     user_input = input("Enter your username and password: ")
-    if len(user_input) != 0:
+    user_pass_list = user_input.split(" ")
+    username_entered = user_pass_list[0]
+    print(type(username_entered))
+    password_entered = user_pass_list[1]
+    print(password_entered)
+    if username_entered in full_dict and full_dict[username_entered] == password_entered:
+        print(f"Correct password, you are now logged in as {username_entered}")
         break
-user_input_name = user_input.split(" ")
-print(f"You entered incorect crediantials for username {user_input_name[0] }, enter password and username again")
+    else:
+        break
+
+# print(f"You entered incorect crediantials for username {user_input_name[0] }, enter password and username again")
 
 
-
-
-
-
-
-
-print(usernames_dict)
+print(full_dict)
